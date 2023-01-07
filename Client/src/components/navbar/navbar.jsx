f434dae6e6e49969a746371534339439098b5575
Pubstar <pieterhoek@msn.com>
2023-01-07 18:43:05 +0100

    Added login to navbar
    TODO: Get familiar with react router and implement logging in

--- Client/src/components/navbar/navbar.jsx
+++ Client/src/components/navbar/navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import ReactLogo from '../../images/react-logo.png';
 
 const navbar = () => {
     return (
         <div className='navbar-container'>
             <div className='links-container'>
+                <img src={ReactLogo} alt="" />
                 <a href="/">Home</a>
+            </div>
+            <div className="login-container">
+                <a href="/">Login</a>
+                <a href="/">Register</a>
             </div>
         </div>
     )
