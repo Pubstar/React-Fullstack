dca65eb2cc76cd777564f07112e574232bf2fa23
Pubstar <pieterhoek@msn.com>
2023-01-09 21:05:09 +0100

    Added in React-router for future login/signup page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,62 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 //components
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/navbar/Navbar';
+import Homepage from './components/homepage/Homepage';
 
 const App = () => {
-
-  let username;
-  let note;
-
-  const [serverData, setServerData] = useState();
-
-  function fetchUsers() {
-    fetch("/api/getUsers")
-      .then(response => response.json()
-        .then(data => setServerData(data)));
-  }
-
-  function createUser(event) {
-    event.preventDefault();
-    fetch("/api/createUser", {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name: username, note: note })
-    })
-    event.target.username.value = "";
-    event.target.note.value = "";
-    fetchUsers();
-  }
-
-  function setUsername(event) {
-    username = event.target.value;
-  }
-
-  function setNote(event) {
-    note = event.target.value;
-  }
-
   // Main page
   // TODO make something relevant instead of fake users
   return (
     <>
       <Navbar />
-      <div className='container'>
-        <h1>Sending / receiving from server</h1>
-        <form onSubmit={createUser}>
-          <input onChange={setUsername} type="text" name="username" id="username" placeholder='username' />
-          <input onChange={setNote} type="text" name="note" id="note" placeholder='note' />
-          <button>Create User!</button>
-        </form>
-        <button onClick={fetchUsers}>Fetch users from DB!</button>
-        {serverData && serverData.map((item, idx) => {
-          return <div key={idx} style={{ padding: 10 + "px" }}>
-            <h3>{item.user}</h3>
-            <p>{item.note}</p>
-          </div>
-        })}
-      </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </>
   )
 }
---
+++ Client/src/components/homepage/Homepage.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import './homepage.css';
+
+const Homepage = () => {
+    let username;
+    let note;
+
+    const [serverData, setServerData] = useState();
+
+    function fetchUsers() {
+        fetch("/api/getUsers")
+            .then(response => response.json()
+                .then(data => setServerData(data)));
+    }
+
+    function createUser(event) {
+        event.preventDefault();
+        fetch("/api/createUser", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: username, note: note })
+        })
+        event.target.username.value = "";
+        event.target.note.value = "";
+        fetchUsers();
+    }
+
+    function setUsername(event) {
+        username = event.target.value;
+    }
+
+    function setNote(event) {
+        note = event.target.value;
+    }
+
+    return (
+        <div className='container'>
+            <h1>Sending / receiving from server</h1>
+            <form onSubmit={createUser}>
+                <input onChange={setUsername} type="text" name="username" id="username" placeholder='username' />
+                <input onChange={setNote} type="text" name="note" id="note" placeholder='note' />
+                <button>Create User!</button>
+            </form>
+            <button onClick={fetchUsers}>Fetch users from DB!</button>
+            {serverData && serverData.map((item, idx) => {
+                return <div key={idx} style={{ padding: 10 + "px" }}>
+                    <h3>{item.user}</h3>
+                    <p>{item.note}</p>
+                </div>
+            })}
+        </div>
+    )
+}
+
+export default Homepage
